7328aa871a4ad3e60e0175b17c0dbc814782dfac
vitya <56838245+vitalik322223@users.noreply.github.com>
2023-04-26 00:19:36 +0300

    Add files via upload

---
+++ laba_2a.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Class1 {
+public:
+    Class1() {};
+    ~Class1() {};
+};
+
+class Class2 : public Class1 {
+public:
+    Class2() {};
+    ~Class2() {};
+};
+void my_copy(vector<Class1*>& classes, Class1* temp) {
+    classes.push_back(temp);
+}
+
+int main()
+{
+    vector<Class1*> classes;
+    Class1 temp1;
+    Class2 temp2;
+    my_copy(classes, &temp1);
+    my_copy(classes, &temp2);
+    for (Class1* Class : classes) {
+        cout << typeid(*Class).name() << std::endl;
+    }
+}
+
